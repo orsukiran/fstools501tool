8985fc643c8656912c946c505f140a8949ce9014
9515056807 <9515056807@KIRAN>
2025-08-08 23:53:55 +0530

    initialllll

---
+++ gitproj03/src/main/java/com/nit/Main/App.java
@@ -0,0 +1,53 @@
+package com.nit.Main;
+
+import oracle.jdbc.OracleDriver;
+
+/**
+ * Hello world!
+ */
+//public class App {
+//	
+//	public static int sum(int a,int b)
+//	{
+//		return a+b;
+//	}
+//	
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        
+//        System.out.println(" Sum of a and b :: "+sum(2,5));
+//        
+//        
+//        
+//    }
+//}
+
+
+//public class App {
+//
+//    public int testsun(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static void main(String[] args) {
+//        App app = new App();
+//        int result = app.testsun(10, 20);
+//        System.out.println("Result: " + result);
+//    }
+//}
+
+
+
+public class App{
+	
+	
+	
+	public static void main(String[] args) {
+		  System.out.println(" hello world ");
+		 OracleDriver d=new OracleDriver();
+		  System.out.println(d.hashCode());
+	}
+}
+
+
+
---
+++ gitproj03/src/test/java/com/nit/Main/AppTest.java
@@ -0,0 +1,32 @@
+package com.nit.Main;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+//public class AppTest {
+//
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue() {
+//        assertTrue(true);
+//    }
+//}
+
+
+//public class AppTest {
+//
+//    @Test
+//    public void testTestSun() {
+//        App app = new App();
+//        int actual = app.testsun(10, 20);
+//        int expected = 30;
+//        assertEquals(expected, actual);
+//    }
+//}
